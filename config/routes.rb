939f14a80edffe4e08d0bbd081feaa9f34bc3f4a
Emily <blancocronos@hotmail.com>
2022-06-12 08:21:44 -0600

    build(new) New project in Ruby on Rails | store online vendelo

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  post '/products', to: 'products#create'
+  get '/products/new', to: 'products#new', as: :new_product
+  get '/products', to: 'products#index'
+  get '/products/:id', to: 'products#show', as: :product
+end
---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+	test 'render a list of products' do
+		get products_path
+
+		assert_response :success
+		assert_select '.product', 2
+	end
+
+	test 'render a detailed product page' do
+		get product_path(products(:ps4))
+
+		assert_response :success
+		assert_select '.title', 'PS4 Fat'
+		assert_select '.description', 'PS4 en buen estado'
+		assert_select '.price', '150'
+	end
+
+	test 'render a new product form' do
+		get new_product_path
+
+		assert_response :success
+		assert_select 'form'
+	end
+
+	test 'allow to create a new product' do
+		post products_path, params: {
+			product: {
+				title: 'Nintendo 64',
+				description: 'Le faltan los cables',
+				price: 45
+			}
+		}
+
+		assert_redirected_to products_path
+	end
+
+end
