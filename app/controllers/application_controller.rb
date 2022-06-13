ccb6e208f6f4a55dcce77dc4d0564d2f4bc59a48
Emily <blancocronos@hotmail.com>
2022-06-13 10:30:30 -0600

    build (work) add categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 class ApplicationController < ActionController::Base
-  around_action :switch_local
+
+  around_action :switch_locale
 
   def switch_locale(&action)
-    I18n.with_locale(locale_form_header, &action)
+    I18n.with_locale(locale_from_header, &action)
   end
 
   private
 
   def locale_from_header
-    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    request.env['HTTP_ACCEPT_LANGUAGE']&.scan(/^[a-z]{2}/)&.first
   end
 end
