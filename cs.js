1ca84d8ef2c13d38311e85dbf0de276671a48b2f
V-Juarez <mytacam78@gmail.com>
2022-06-17 09:12:40 -0600

    Constantes

---
+++ cs.js
@@ -0,0 +1,24 @@
+let a;
+const PI = 3.1416;
+
+console.log(PI);
+
+a = "Kenai"
+console.log(a)
+
+let objeto = {
+  nombre: "John",
+  edad: 35,
+}
+console.log(objeto)
+
+let colores = ["Blanco", "Negro", "azul"]
+
+objeto.correo = "jonmircha@mail.com"
+objeto.profesion = "Backend"
+
+colores.push("Anaranjado")
+colores.push("White")
+
+console.log(objeto)
+console.log(colores)
