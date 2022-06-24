14e2875b194e992c3e9a75eb2dc80c72456dbbdc
V-Juarez <mytacam78@gmail.com>
2022-06-23 19:24:42 -0600

    Strings

---
+++ ruby_3_Beginner/chapter_3/5.string.rb
@@ -0,0 +1,26 @@
+=begin 
+Expression
+Output
+"Test" + "Test"
+TestTest
+"test".capitalize
+Test
+"Test".downcase
+test
+"Test".chop
+Tes
+"Test".next
+Tesu
+"Test".reverse
+tseT
+"Test".sum
+416
+"Test".swapcase
+tEST
+"Test".upcase
+TEST
+"Test".upcase.reverse
+TSET
+"Test".upcase.reverse.next
+TSEU
+=end
