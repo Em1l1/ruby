a01cdc8a36ddaf8ee0f326b9889adf7b149619c0
V-Juarez <mytacam78@gmail.com>
2022-06-24 12:44:07 -0600

    Ternary Operator

---
+++ ruby_3_Beginner/chapter_3/15.ternary_operator.rb
@@ -0,0 +1,8 @@
+puts "?, The Ternary Operator"
+age = 19
+type = age < 18 ? "child" : "adult"
+puts "You are a " + type
+
+puts "Second options"
+age = 12
+puts "You are a " + (age < 18 ? "child" : "adult")
