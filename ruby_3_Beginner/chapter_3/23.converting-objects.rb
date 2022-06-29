d59640ae107a787d1562382457ed2c096976f7a7
Emily <brancocronos@hotmail.com>
2022-06-29 08:57:37 -0400

    Converted Co-authored-by @V-Juarez <mytamca78@gmail.com>

---
+++ ruby_3_Beginner/chapter_3/23.converting-objects.rb
@@ -0,0 +1,13 @@
+puts "12" + "10"
+puts 12 + 10
+
+puts 'Converting'
+puts "12".to_i + "10".to_i
+puts 12.to_s + 10.to_s
+
+# Methods 
+# Integer       => .to_i
+# Float         => .to_f
+# String class  => .to_sym
+# string        => .to_s
+
